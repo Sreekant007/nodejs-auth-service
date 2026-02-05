059440196ff241e502973116136683d1e8cc58a3
Shrikant <govandeshrikant6@gmail.com>
2026-02-05 12:39:56 +0530

    feat: setup TypeScript Node.js server with Express

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+/* Global Middleware */
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/health-check", (_req, res) => {
+  res.status(200).send("OK");
+});
+
+export default app;
